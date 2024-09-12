d7cd55de352c50b955c95202edd4d7108c6b26e5
Vinay Malyala <srivinay1432@gmail.com>
2024-09-12 21:18:28 +0530

    added scroll up & footer

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ navLink.forEach(link => {
         hamburger.classList.toggle("ri-close-large-line");
     });
 });
+
+// show scroll up 
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+
+    if(this.scrollY >= 250){
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-10");
+    }else{
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-10");
+    }
+}
+window.addEventListener("scroll", scrollUp);
